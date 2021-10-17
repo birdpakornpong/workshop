f57b72d85c20a2091b867ba28e51bd1a8f96bcb8
KHUNBIRD <birdpakornpong@gmail.com>
2021-10-17 20:57:33 +0700

    text

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,8 +1,12 @@
 import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { HealthCheckService, HealthCheck, HealthIndicatorResult } from '@nestjs/terminus';
+import {
+  HealthCheckService,
+  HealthCheck,
+  HealthIndicatorResult,
+} from '@nestjs/terminus';
 import { HealthCheckResult } from '@nestjs/terminus/dist/health-check';
-import { Public } from 'artifacts/auth/metadata/public.metadata';
+// import { Public } from 'artifacts/auth/metadata/public.metadata';
 import { AppHealthIndicator } from './app.health';
 import { RootPathResDto } from './dto/responses/root-path-res.dto';
 
@@ -14,22 +18,30 @@ export class HealthController {
     private appHealthIndicator: AppHealthIndicator,
   ) {}
 
-  @Public()
+  // @Public()
   @ApiOperation({ summary: 'Get app name' })
-  @ApiResponse({ status: HttpStatus.OK, description: 'Call root path and return app name', type: RootPathResDto })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Call root path and return app name',
+    type: RootPathResDto,
+  })
   @Get()
   public rootPath(): RootPathResDto {
     return new RootPathResDto(process.env.APP_NAME);
   }
 
-  @Public()
+  // @Public()
   @ApiOperation({ summary: 'Get API status' })
-  @ApiResponse({ status: HttpStatus.OK, description: 'Check API and database status' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Check API and database status',
+  })
   @Get('/v1/health')
   @HealthCheck()
   public readiness(): Promise<HealthCheckResult> {
     return this.healthService.check([
-      async (): Promise<HealthIndicatorResult> => this.appHealthIndicator.isHealthy('app'),
+      async (): Promise<HealthIndicatorResult> =>
+        this.appHealthIndicator.isHealthy('app'),
     ]);
   }
 }
